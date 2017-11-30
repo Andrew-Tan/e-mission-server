486c98c6683f115cd3b9235b4ccf3b80d62331ec
tan95 <andrew--tan@outlook.com>
2017-11-30 15:00:27 -0800

    fix wrapper

--- emission/net/usercache/formatters/android/survey.py
+++ emission/net/usercache/formatters/android/survey.py
@@ -1,26 +1,5 @@
 import logging
-import arrow
-
-import emission.net.usercache.formatters.common as fc
-import emission.storage.decorations.local_date_queries as ecsdlq
-import attrdict as ad
+import emission.net.usercache.formatters.generic.userlabel as fgl
 
 def format(entry):
-    formatted_entry = ad.AttrDict()
-    formatted_entry["_id"] = entry["_id"]
-    formatted_entry.user_id = entry.user_id
-
-    metadata = entry.metadata
-    if "time_zone" not in metadata:
-        metadata.time_zone = "America/Los_Angeles"
-    if "write_ts" in metadata:
-        logging.debug("Timestamp conversion: %s -> %s done" % (metadata.write_ts, metadata.write_ts))
-        fc.expand_metadata_times(metadata)
-    formatted_entry.metadata = metadata
-
-    data = entry.data
-    if "write_ts" in metadata:
-        fc.expand_data_times(data, metadata)
-    formatted_entry.data = data
-
-    return formatted_entry
+    return fgl.format(entry)
